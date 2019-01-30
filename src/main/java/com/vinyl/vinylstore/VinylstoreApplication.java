ee58093c381d0294dfafdf7ca76f55caac7b768b
Raluca Simionescu <ralusimi97@gmail.com>
2019-01-30 13:51:44 +0200

    initial commit

---
+++ src/main/java/com/vinyl/vinylstore/VinylstoreApplication.java
@@ -0,0 +1,14 @@
+package com.vinyl.vinylstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylstoreApplication.class, args);
+	}
+
+}
+
