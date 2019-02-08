edab0d5dd869f6b35bd2760d21607d9ed6344989
Raluca Simionescu <ralusimi97@gmail.com>
2019-02-08 18:00:42 +0200

    moved existing email validation to the validator package

--- src/main/java/com/vinyl/service/validation/UserEmailValidator.java
+++ src/main/java/com/vinyl/service/validation/UserEmailValidator.java
@@ -3,10 +3,18 @@ package com.vinyl.service.validation;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import com.vinyl.model.User;
+import com.vinyl.repository.UserRepository;
 import com.vinyl.service.exception.BadRequestException;
 
+@Component
 public class UserEmailValidator implements Validator<User> {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	private static final String VALID_EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
@@ -20,6 +28,9 @@ public class UserEmailValidator implements Validator<User> {
 		if (!matcher.find()) {
 			throw new BadRequestException("The email address is invalid!");
 		}
-	}
 
+		if (userRepository.findByEmail(user.getEmail()) != null) {
+			throw new BadRequestException("Email address already exists!");
+		}
+	}
 }
--- src/main/java/com/vinyl/service/validation/ValidatorFactory.java
+++ src/main/java/com/vinyl/service/validation/ValidatorFactory.java
@@ -1,5 +1,6 @@
 package com.vinyl.service.validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.vinyl.model.User;
@@ -7,15 +8,19 @@ import com.vinyl.model.User;
 @Component
 public class ValidatorFactory {
 
+	@Autowired
+	private UserEmailValidator emailValidator;
+	
 	public Validator<User> getUserNameValidator() {
 		return new UserNameValidator();
 	}
 
 	public Validator<User> getUserEmailValidator() {
-		return new UserEmailValidator();
+		return emailValidator;
 	}
 
 	public Validator<User> getUserPasswordValidator() {
 		return new UserPasswordValidator();
 	}
+
 }
