cdec6b5f5aca7bc725188313099df2d8e7eb907d
Raluca Simionescu <ralusimi97@gmail.com>
2019-02-11 09:58:37 +0200

    changed email validator to be constructed by factory instead of spring

--- src/main/java/com/vinyl/service/validation/UserEmailValidator.java
+++ src/main/java/com/vinyl/service/validation/UserEmailValidator.java
@@ -3,22 +3,23 @@ package com.vinyl.service.validation;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import com.vinyl.model.User;
 import com.vinyl.repository.UserRepository;
 import com.vinyl.service.exception.BadRequestException;
 
-@Component
 public class UserEmailValidator implements Validator<User> {
 
-	@Autowired
-	private UserRepository userRepository;
+	private final UserRepository userRepository;
 
 	private static final String VALID_EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 	private static final Pattern EMAIL_PATTERN = Pattern.compile(VALID_EMAIL_REGEX);
+
+	
+	public UserEmailValidator(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 
 	@Override
 	public void validate(User user) {
--- src/main/java/com/vinyl/service/validation/ValidatorFactory.java
+++ src/main/java/com/vinyl/service/validation/ValidatorFactory.java
@@ -4,19 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.vinyl.model.User;
+import com.vinyl.repository.UserRepository;
 
 @Component
 public class ValidatorFactory {
 
-	@Autowired
-	private UserEmailValidator emailValidator;
+	private final UserRepository userRepository;
 	
+	@Autowired
+	public ValidatorFactory(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
 	public Validator<User> getUserNameValidator() {
 		return new UserNameValidator();
 	}
 
 	public Validator<User> getUserEmailValidator() {
-		return emailValidator;
+		return new UserEmailValidator(userRepository);
 	}
 
 	public Validator<User> getUserPasswordValidator() {
