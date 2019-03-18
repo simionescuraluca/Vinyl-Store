d6a402cd81a9c986140e0bb713f56a5f619d3126
Raluca Simionescu <ralusimi97@gmail.com>
2019-03-18 16:40:11 +0200

    log errors to log file & rollover each day

--- src/main/java/com/vinyl/service/validation/UserEmailValidator.java
+++ src/main/java/com/vinyl/service/validation/UserEmailValidator.java
@@ -3,16 +3,21 @@ package com.vinyl.service.validation;
 import com.vinyl.model.User;
 import com.vinyl.repository.UserRepository;
 import com.vinyl.service.exception.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserEmailValidator implements Validator<User> {
 
-    private static final String VALID_EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    private static final Pattern EMAIL_PATTERN = Pattern.compile(VALID_EMAIL_REGEX);
-    private final UserRepository userRepository;
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserEmailValidator.class);
+
+	private final UserRepository userRepository;
+
+	private static final String VALID_EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	private static final Pattern EMAIL_PATTERN = Pattern.compile(VALID_EMAIL_REGEX);
 
 
     public UserEmailValidator(UserRepository userRepository) {
@@ -24,11 +29,10 @@ public class UserEmailValidator implements Validator<User> {
 
         Matcher matcher = EMAIL_PATTERN.matcher(user.getEmail());
 
-        if (!matcher.find()) {
-            throw new BadRequestException("The email address is invalid!");
-        }
-        userRepository.findByEmail(user.getEmail()).ifPresent((e) -> {
-            throw new BadRequestException("Email address already exists!");
-        });
-    }
+		if (!matcher.find()) {
+			LOGGER.error("Invalid Email Error");
+			throw new BadRequestException("The email address is invalid!");
+		}
+		userRepository.findByEmail(user.getEmail()).ifPresent((e) -> {throw new BadRequestException("Email address already exists!");});
+	}
 }
--- src/main/java/com/vinyl/service/validation/UserNameValidator.java
+++ src/main/java/com/vinyl/service/validation/UserNameValidator.java
@@ -2,14 +2,18 @@ package com.vinyl.service.validation;
 
 import com.vinyl.model.User;
 import com.vinyl.service.exception.BadRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserNameValidator implements Validator<User> {
 
-    private static final String VALID_NAME_REGEX = "^[A-Z][a-zA-Z]+$";
-    private static final Pattern NAME_PATTERN = Pattern.compile(VALID_NAME_REGEX);
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserNameValidator.class);
+
+	private static final String VALID_NAME_REGEX = "^[A-Z][a-zA-Z]+$";
+	private static final Pattern NAME_PATTERN = Pattern.compile(VALID_NAME_REGEX);
 
     @Override
     public void validate(User user) {
@@ -17,9 +21,10 @@ public class UserNameValidator implements Validator<User> {
         Matcher matcher1 = NAME_PATTERN.matcher(user.getFirstName());
         Matcher matcher2 = NAME_PATTERN.matcher(user.getSecondName());
 
-        if (!matcher1.find() || !matcher2.find()) {
-            throw new BadRequestException("The names should start with capital letter and have more than 1 character!");
-        }
-    }
+		if (!matcher1.find() || !matcher2.find()) {
+			LOGGER.error("Invalid Name Error");
+			throw new BadRequestException("The names should start with capital letter and have more than 1 character!");
+		}
+	}
 
 }
