efda896683cb3d817b3a4193fe63852a30a5e9a6
Raluca Simionescu <ralusimi97@gmail.com>
2019-04-02 10:04:55 +0300

    get inventory complete endpoint

--- src/main/java/com/vinyl/repository/ProductRepository.java
+++ src/main/java/com/vinyl/repository/ProductRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends CrudRepository<Product, Integer> {
-   // List<InventoryDTO> getInventory();
     List<Product> findAll();
 }
